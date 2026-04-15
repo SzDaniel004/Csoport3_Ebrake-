5c046b50f5e4d9c9b8562279ac0c7c83b9dca1d3
Balazs0405 <balazsbotyanszki001@gmail.com>
2026-04-15 20:02:34 +0200

    v1

---
+++ code/controller_node.hpp
@@ -0,0 +1,31 @@
+#ifndef CONTROLLER__CONTROLLER_NODE_HPP_
+#define CONTROLLER__CONTROLLER_NODE_HPP_
+
+#include "rclcpp/rclcpp.hpp"
+#include "std_msgs/msg/bool.hpp"
+#include "std_msgs/msg/float32.hpp"
+
+class ControllerNode : public rclcpp::Node
+{
+public:
+  explicit ControllerNode();
+
+private:
+  rclcpp::Subscription<std_msgs::msg::Bool>::SharedPtr brake_command_subscription_;
+  rclcpp::Subscription<std_msgs::msg::Float32>::SharedPtr distance_subscription_;
+  rclcpp::Subscription<std_msgs::msg::Float32>::SharedPtr brake_distance_subscription_;
+
+  rclcpp::Publisher<std_msgs::msg::Float32>::SharedPtr brake_output_publisher_;
+
+  bool is_braking_ = false;
+
+  double current_distance_ = 0.0;
+  double brake_distance_ = 0.0;
+
+  void brake_command_callback(const std_msgs::msg::Bool & msg);
+  void distance_callback(const std_msgs::msg::Float32 & msg);
+  void brake_distance_callback(const std_msgs::msg::Float32 & msg);
+  void apply_brake(bool brake_active);
+};
+
+#endif  // CONTROLLER__CONTROLLER_NODE_HPP_
